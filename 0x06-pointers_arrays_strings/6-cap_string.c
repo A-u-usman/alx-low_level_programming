d2d11e3030926bc14075dc2b2f3601933fe6bc7d
Muhammad Abdullahi <51286052+A-u-usman@users.noreply.github.com>
2022-09-24 04:45:57 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 /**
  * cap_string - capitalizes all words of a string
  * @s: input string.
@@ -28,4 +29,3 @@ char *cap_string(char *s)
 	}
 	return (s);
 }
-Foo
